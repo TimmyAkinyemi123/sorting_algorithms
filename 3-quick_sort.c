700e7793ae7b3802a0b1353a5d30166d9eacd7ed
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-10-20 07:14:19 +0100

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void quicksort(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using
+ * the Quick sort algorithm with the Lomuto partition scheme.
+ * @array: The array to be sorted.
+ * @size: The number of elements in the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (size < 2)
+        return;
+
+    quicksort(array, 0, size - 1, size);
+}
+
+/**
+ * quicksort - Helper function for quick_sort.
+ * @array: The array to be sorted.
+ * @low: The low index of the partition to be sorted.
+ * @high: The high index of the partition to be sorted.
+ * @size: The size of the full array.
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int pi = lomuto_partition(array, low, high, size);
+        quicksort(array, low, pi - 1, size);
+        quicksort(array, pi + 1, high, size);
+    }
+}
+
+/**
+ * lomuto_partition - Partitions the array using Lomuto partition scheme.
+ * @array: The array to be sorted.
+ * @low: The low index of the partition.
+ * @high: The high index of the partition.
+ * @size: The size of the full array.
+ * Return: The index of the pivot element.
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1, j, temp;
+
+    for (j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            if (i != j)
+                print_array(array, size);
+        }
+    }
+
+    temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+    if (i + 1 != high)
+        print_array(array, size);
+
+    return (i + 1);
+}
