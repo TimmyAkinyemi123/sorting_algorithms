c534c0c29a7aa04a0387d756a0d18bcf86c0c978
Timmy <akinyemitimilehin044@gmail.com>
2023-10-19 23:23:17 -0700

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,10 +13,10 @@ int lomuto_partition(int *array, int low, int high, size_t size);
  */
 void quick_sort(int *array, size_t size)
 {
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    quicksort(array, 0, size - 1, size);
+	quicksort(array, 0, size - 1, size);
 }
 
 /**
@@ -28,12 +28,14 @@ void quick_sort(int *array, size_t size)
  */
 void quicksort(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pi = lomuto_partition(array, low, high, size);
-        quicksort(array, low, pi - 1, size);
-        quicksort(array, pi + 1, high, size);
-    }
+	int pi;
+
+	if (low < high)
+	{
+		pi = lomuto_partition(array, low, high, size);
+		quicksort(array, low, pi - 1, size);
+		quicksort(array, pi + 1, high, size);
+	}
 }
 
 /**
@@ -46,27 +48,29 @@ void quicksort(int *array, int low, int high, size_t size)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1, j, temp;
+	int pivot = array[high];
+	int i = low - 1, j, temp;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            if (i != j)
-                print_array(array, size);
-        }
-    }
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
 
-    temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
-    if (i + 1 != high)
-        print_array(array, size);
+			if (i != j)
+				print_array(array, size);
+		}
+	}
 
-    return (i + 1);
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+
+	if (i + 1 != high)
+		print_array(array, size);
+
+	return (i + 1);
 }
