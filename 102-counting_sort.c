7e12bc56c368a758af5fedb2453a12fe47f29c4f
TimmyAkinyemi123 <120695688+TimmyAkinyemi123@users.noreply.github.com>
2023-10-21 20:51:19 +0100

    6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,52 +1,52 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using
- * the Counting sort algorithm.
+ * counting_sort - Sorts an array of integers in ascending order using Counting sort.
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, max = array[0];
-	int *output_array, *count_array;
-	size_t j;
+    if (array == NULL || size <= 1)
+        return;
 
-	if (size <= 1)
-		return;
+    int max = array[0];
+    for (size_t i = 1; i < size; i++) {
+        if (array[i] > max)
+            max = array[i];
+    }
 
-	for (j = 1; j < size; j++)
-	{
-		if (array[j] > max)
-			max = array[j];
-	}
-	count_array = malloc(sizeof(int) * (max + 1));
-	if (count_array == NULL)
-		return;
+    int *count = malloc((max + 1) * sizeof(int));
+    if (count == NULL)
+        return;
 
-	for (i = 0; i <= max; i++)
-		count_array[i] = 0;
-	for (j = 0; j < size; j++)
-		count_array[array[j]]++;
+    for (int i = 0; i <= max; i++) {
+        count[i] = 0;
+    }
 
-	print_array(count_array, max + 1);
-	for (i = 1; i <= max; i++)
-		count_array[i] += count_array[i - 1];
+    for (size_t i = 0; i < size; i++) {
+        count[array[i]]++;
+    }
 
-	output_array = malloc(sizeof(int) * size);
-	if (output_array == NULL)
-	{
-		free(count_array);
-		return;
-	}
-	for (i = size - 1; i >= 0; i--)
-	{
-		output_array[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
-	}
-	for (j = 0; j < size; j++)
-		array[j] = output_array[j];
-	free(output_array);
-	free(count_array);
+    /* Print the counting array */
+    for (int i = 0; i <= max; i++) {
+        if (i != 0) {
+            printf(", ");
+        }
+        printf("%d", count[i]);
+    }
+    printf("\n");
+
+    int index = 0;
+    for (int i = 0; i <= max; i++) {
+        while (count[i] > 0) {
+            array[index] = i;
+            index++;
+            count[i]--;
+        }
+    }
+
+    free(count);
 }
