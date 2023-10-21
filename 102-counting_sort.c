acf10ec5c7e3dd5e6cd88e1fbf4dc362e0e51b44
Timmy <akinyemitimilehin044@gmail.com>
2023-10-21 13:09:10 -0700

    task 6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -3,50 +3,49 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array of integers in ascending order using Counting sort.
+ * counting_sort - Sorts an array of integers in
+ * ascending order using Counting sort.
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
-    if (array == NULL || size <= 1)
-        return;
+	int i, max = array[0], *count, index = 0;
+	size_t j;
 
-    int max = array[0];
-    for (size_t i = 1; i < size; i++) {
-        if (array[i] > max)
-            max = array[i];
-    }
+	if (array == NULL || size <= 1)
+		return;
+	for (j = 1; j < size; j++)
+	{
+		if (array[j] > max)
+			max = array[j];
+	}
 
-    int *count = malloc((max + 1) * sizeof(int));
-    if (count == NULL)
-        return;
+	count = malloc((max + 1) * sizeof(int));
+	if (count == NULL)
+		return;
+	for (i = 0; i <= max; i++)
+		count[i] = 0;
+	for (j = 0; j < size; j++)
+		count[array[j]]++;
 
-    for (int i = 0; i <= max; i++) {
-        count[i] = 0;
-    }
+	/* Print the counting array */
+	for (i = 0; i <= max; i++)
+	{
+		if (i != 0)
+			printf(", ");
+		printf("%d", count[i]);
+	}
+	printf("\n");
 
-    for (size_t i = 0; i < size; i++) {
-        count[array[i]]++;
-    }
-
-    /* Print the counting array */
-    for (int i = 0; i <= max; i++) {
-        if (i != 0) {
-            printf(", ");
-        }
-        printf("%d", count[i]);
-    }
-    printf("\n");
-
-    int index = 0;
-    for (int i = 0; i <= max; i++) {
-        while (count[i] > 0) {
-            array[index] = i;
-            index++;
-            count[i]--;
-        }
-    }
-
-    free(count);
+	for (i = 0; i <= max; i++)
+	{
+		while (count[i] > 0)
+		{
+			array[index] = i;
+			index++;
+			count[i]--;
+		}
+	}
+	free(count);
 }
